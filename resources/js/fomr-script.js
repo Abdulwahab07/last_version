8dc9a8f3013c7afa00aa4124ee0e3b9d04048a9b
Abdulwahab <arefdan@hotmail.com>
2019-06-12 15:32:35 +0300

    my HTML Projects

---
+++ resources/js/fomr-script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	
+	/*	main navigation  */
+
+	
+	/* mobile navi */ 
+	
+$('.lang').click(function(){
+		var icon = $('.contact-form div');
+		
+		//nav.slideToggle(500);
+		if(icon.hasClass('lang-dir')){
+			icon.removeClass('lang-dir');
+		}else{
+			icon.addClass('lang-dir');
+		}
+		
+		
+		
+	});
+	
+	
+	
+});
+
+
+	
